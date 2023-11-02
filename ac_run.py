d2bd1d5054911e982cafdf1a64a90f8636f62502
Davide Buoso <daviibuoso@gmail.com>
2023-11-02 08:57:48 +0100

    working

--- ac_run.py
+++ ac_run.py
@@ -3,12 +3,12 @@ import matplotlib.pyplot as plt
 from stable_baselines3 import DQN
 from stable_baselines3.common.env_checker import check_env
 from agents.actor_critic import Agent
-import wandb
-
+import numpy as np
 env = RoverEnvV2()
 agent = Agent(3,n_actions=8)
 N_GAMES = 1000
 load_checkpoint = False
+score_history = []
 for i in range(N_GAMES):
     observation = env.reset()[0]
     score=0
@@ -21,7 +21,8 @@ for i in range(N_GAMES):
             agent.learn(observation, reward, observation_, done)
         observation = observation_
         #env.render()
-    print(f"Episode: {i}, Reward: {score}, Visited: {env.cells_visited}, Targets Collected: {env.targets_collected}")
+    score_history.append(score)
+    print(f"Episode: {i}, Avg Reward: {np.mean(score_history[-20:])}, Visited: {env.cells_visited}, Targets Collected: {env.targets_collected}")
     # Render the game
 
 #env.close()
