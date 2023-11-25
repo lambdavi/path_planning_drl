830fc60e2b8ce6f96d2c24a2e0e835aa314dc004
Davide Buoso <daviibuoso@gmail.com>
2023-11-25 14:00:36 +0100

    ppo

--- run.py
+++ run.py
@@ -73,8 +73,7 @@ if args.sb:
                     ent_coef = 0.01,
                     policy_kwargs=dict(normalize_images=False), 
                     tensorboard_log=log_dir, 
-                    verbose=0, 
-                    device='cpu' if policy=="MlpPolicy" else "auto")
+                    verbose=0)
         
     # Train the agent
     if LOG_ON:
