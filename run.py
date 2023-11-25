544734defcabb23ea7b3ee8046fe91f4e51cb838
Davide Buoso <daviibuoso@gmail.com>
2023-11-25 13:46:59 +0100

    ppo

--- run.py
+++ run.py
@@ -48,11 +48,33 @@ if args.sb:
     policy = "CnnPolicy" if args.obs == "image" else "MlpPolicy"
     print(policy)
     if args.algo == "dqn":
-        model = DQN(env=env, policy=policy, exploration_fraction=0.5, policy_kwargs=dict(normalize_images=False), tensorboard_log=log_dir, verbose=0, buffer_size=10000 if policy=='MlpPolicy' else 100, device='cpu' if policy=="MlpPolicy" else "auto")
+        model = DQN(env=env, 
+                    policy=policy, 
+                    exploration_fraction=0.5, 
+                    policy_kwargs=dict(normalize_images=False), 
+                    tensorboard_log=log_dir, 
+                    verbose=0, 
+                    buffer_size=10000 if policy=='MlpPolicy' else 100, 
+                    device='cpu' if policy=="MlpPolicy" else "auto")
     elif args.algo == "a2c":
-        model = A2C(env=env, policy=policy, policy_kwargs=dict(normalize_images=False), tensorboard_log=log_dir, verbose=0, device='cpu' if policy=="MlpPolicy" else "auto")
+        model = A2C(env=env, 
+                    policy=policy, 
+                    policy_kwargs=dict(normalize_images=False), 
+                    tensorboard_log=log_dir, 
+                    verbose=0, 
+                    device='cpu' if policy=="MlpPolicy" else "auto")
     else:
-        model = PPO(env=env, policy=policy, policy_kwargs=dict(normalize_images=False), tensorboard_log=log_dir, verbose=0, device='cpu' if policy=="MlpPolicy" else "auto")
+        model = PPO(env=env, 
+                    policy=policy, 
+                    n_steps=1024, 
+                    n_epochs=4, 
+                    gamma = 0.999,
+                    gae_lambda = 0.98,
+                    ent_coef = 0.01,
+                    policy_kwargs=dict(normalize_images=False), 
+                    tensorboard_log=log_dir, 
+                    verbose=0, 
+                    device='cpu' if policy=="MlpPolicy" else "auto")
         
     # Train the agent
     if LOG_ON:
